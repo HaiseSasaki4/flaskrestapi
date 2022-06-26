07d1b262c135772aa3d042f5c3c2e02c0d58b3a8
HaiseSasaki4 <alexandrefrancis18@gmail.com>
2022-06-26 12:52:41 +0530

    flask code added

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Super api working")
+from flask import Flask, request
+from flask_cors  import cross_origin, CORS
+from flask_restful import Resource, Api, abort, reqparse
+
+app = Flask(_name_)
+api = Api(app)
+
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+
+class Employees(Resource):           
+    def get(self):
+        return {'employees': [{'id':1, 'name':'Balram'},{'id':2, 'name':'Tom'}]} 
+
+api.add_resource(Employees, '/employees') # Route_1
+    
+
+
+if _name_ == '_main_':
+    app.run(port=5050)
